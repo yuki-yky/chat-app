cd811fd4976d019e188a714ca4531605ead336d1
yuki-yky <gokuenmal@gmail.com>
2026-07-08 12:49:10 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   resources :users, only: [:edit, :update]
-  # ↑↑上の記述は削除しましょう↑↑
+  resources :rooms, only: [:new, :create]
   root to: "messages#index"
 end
