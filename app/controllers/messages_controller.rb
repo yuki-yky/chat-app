5cd0200398eb0c00095e0d4adc90a1d6343e4704
yuki-yky <gokuenmal@gmail.com>
2026-07-31 12:57:59 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,8 @@ class MessagesController < ApplicationController
   def index
   @message = Message.new
   @room = Room.find(params[:room_id])
-  end
+ @messages = @room.messages.includes(:user)  
+end
 
  def create
     @room = Room.find(params[:room_id])
@@ -10,6 +11,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+       @messages = @room.messages.includes(:user)
       render :index, status: :unprocessable_entity
     end
   end
